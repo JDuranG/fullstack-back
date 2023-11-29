e72ff7f5e1b47be55a570cbfe8806500df6c3749
JDuranG <javier.duran@getronics.com>
2023-11-29 14:53:23 +0800

    Pytest v1
    P`ytest fail

--- persistencia.py
+++ persistencia.py
@@ -10,5 +10,5 @@ def guardar_pedido(nombre, apellidos):
     """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-        file.write(nombre + " " + apellidos + " ---- " + date + "\n")
+        file.write("-" + nombre + " " + apellidos + " ---- " + date + "\n")
         file.close()
