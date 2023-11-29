4c2df08dfa6ab20eea755ba76a7eb4cb1bed4aa1
JDuranG <javier.duran@getronics.com>
2023-11-29 14:55:14 +0800

    pytest v2

--- persistencia.py
+++ persistencia.py
@@ -2,13 +2,10 @@
 Persistencia
 """
 
-from datetime import datetime
-
 def guardar_pedido(nombre, apellidos):
     """
     Guardar pedido en un txt
     """
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        date = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-        file.write("-" + nombre + " " + apellidos + " ---- " + date + "\n")
+        file.write("-" + nombre + " " + apellidos + "\n")
         file.close()
